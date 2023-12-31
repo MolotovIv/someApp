df95fc4177ee0d266019ae922c8d62302d7cc308
Ivan Ponosov <ponosov.v49@gmail.com>
2023-12-31 12:41:50 +0500

    code splitting and router added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { Counter } from "./src/Components/Counter"
 import './index.scss'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import AboutPage from "./src/Pages/About/AboutPage"
+import MainPage from "./src/Pages/Main/MainPage"
+import { Link } from "react-router-dom"
+import { AboutPageAsync } from "./src/Pages/About/AboutPage.async"
+import { MainPageAsync } from "./src/Pages/Main/MainPage.async"
+import { Suspense } from "react"
 
 export const App = () => {
 	return (
-		<div className="app">sadasdasf
-			<Counter />
+		<div className="app">
+			<Link to={'/'}>Главная</Link>
+			<Link to={'about'}>Обо всем</Link>
+			<Suspense fallback={<div>Loading</div>}>
+				<Routes>
+					<Route path="about" element={<AboutPageAsync />} />
+					<Route path="/" element={<MainPageAsync />} />
+				</Routes>
+			</Suspense>
+			{/* <Counter /> */}
 		</div>
 	)
 }
--- config/build/buildWebpackDevServer.ts
+++ config/build/buildWebpackDevServer.ts
@@ -5,7 +5,8 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
 
 	return {
 		port: options.port,
-		open: true
+		open: true,
+		historyApiFallback: true
 	}
 
 }
